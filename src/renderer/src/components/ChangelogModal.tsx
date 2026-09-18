4c83b48713dc4bac2880bcce877d64bb9aed0bd5
João Leite <joao.leite@wiremaze.com>
2026-09-18 17:36:10 +0100

    chore(release): 1.12.0

--- src/renderer/src/components/ChangelogModal.tsx
+++ src/renderer/src/components/ChangelogModal.tsx
@@ -17,7 +17,7 @@ const CHANGELOG: Entry[] = [
   {
     version: '1.12.0',
     date: '2026-09-18',
-    tag: 'new',
+    tag: 'latest',
     sections: [
       {
         title: 'Fixes',
@@ -43,7 +43,6 @@ const CHANGELOG: Entry[] = [
   {
     version: '1.11.0',
     date: '2026-09-18',
-    tag: 'latest',
     sections: [
       {
         title: 'Features',
