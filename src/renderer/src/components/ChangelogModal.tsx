1ce74623f67418d706a38b81df59a96b1c5a5517
João Leite <joao.leite@wiremaze.com>
2026-09-21 17:23:16 +0100

    chore: begin 1.15.0 (unreleased)

--- src/renderer/src/components/ChangelogModal.tsx
+++ src/renderer/src/components/ChangelogModal.tsx
@@ -15,6 +15,12 @@ interface Entry {
 
 const CHANGELOG: Entry[] = [
   {
+    version: '1.15.0',
+    date: '2026-09-21',
+    tag: 'new',
+    sections: []
+  },
+  {
     version: '1.14.0',
     date: '2026-09-21',
     tag: 'latest',
